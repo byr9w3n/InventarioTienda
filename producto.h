bb8345a14b8088f866cbe3acb9d8c1957c8c6b88
Byron <byronvalencia676@gmail.com>
2026-01-11 20:06:34 -0500

    commit 3

--- producto.h
+++ producto.h
@@ -1,4 +1,12 @@
 #ifndef PRODUCTO_H
 #define PRODUCTO_H
+#include <QString>
+
+struct Producto {
+    QString nombre;
+    QString talla;
+    double precio;
+    int stock;
+};
 
 #endif // PRODUCTO_H

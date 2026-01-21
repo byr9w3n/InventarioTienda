8d191aaf53aebab0e8c9a699b830fc42f47ea10d
Byron <byronvalencia676@gmail.com>
2026-01-20 22:19:16 -0500

    Dejar de rastrear la carpeta build y archivos temporales

--- build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/deploy_InventarioTienda_599beb0bfe.cmake
+++
@@ -1,8 +0,0 @@
-include(C:/Users/byron/Documents/InventarioTienda/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/InventarioTienda-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_ALL_MODULES_FOUND_VIA_FIND_PACKAGE "ZlibPrivate;EntryPointPrivate;Core;Gui;Widgets")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/byron/Documents/InventarioTienda/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/InventarioTienda.exe
-    GENERATE_QT_CONF
-)
